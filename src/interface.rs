b01e5d7916edc2bdf9a89479b60cb19a30074ec7
Star Brilliant <m13253@hotmail.com>
2014-12-31 16:43:16 +0800

    Update

--- src/interface.rs
+++ src/interface.rs
@@ -8,7 +8,8 @@ pub type Complex = binding::kiss_fft_cpx;
 
 pub struct KissFFT {
     cfg: binding::kiss_fft_cfg,
-    nfft: uint
+    nfft: uint,
+    nfft_rsqrt: Scalar
 }
 
 impl KissFFT {
@@ -19,22 +20,38 @@ impl KissFFT {
         assert!(cfg != std::ptr::null_mut());
         KissFFT {
             cfg: cfg,
-            nfft: nfft
+            nfft: nfft,
+            nfft_rsqrt: std::num::Float::rsqrt(nfft as Scalar)
         }
     }
-    pub fn transform(&mut self, fin: &[Complex], fout: &mut [Complex]) {
+    pub fn transform<'a>(&'a mut self, fin: &[Complex], fout: &mut [Complex]) -> &'a mut KissFFT {
         assert!(fin.len() >= self.nfft);
         assert!(fout.len() >= self.nfft);
         assert!(self.cfg != std::ptr::null_mut());
         unsafe {
             binding::kiss_fft(self.cfg, fin.as_ptr(), fout.as_mut_ptr())
         }
+        self
     }
     pub fn transform_to_vec(&mut self, fin: &[Complex]) -> Vec<Complex> {
         assert!(fin.len() >= self.nfft);
         assert!(self.cfg != std::ptr::null_mut());
         let mut result = Vec::from_elem(self.nfft, Complex {r: 0., i: 0.});
         self.transform(fin, result.as_mut_slice());
+        result
+    }
+    pub fn transform_norm<'a>(&'a mut self, fin: &[Complex], fout: &mut [Complex]) -> &'a mut KissFFT {
+        self.transform(fin, fout);
+        for i in fout.iter_mut() {
+            *i = *i * self.nfft_rsqrt
+        }
+        self
+    }
+    pub fn transform_norm_to_vec(&mut self, fin: &[Complex]) -> Vec<Complex> {
+        let mut result = self.transform_to_vec(fin);
+        for i in result.iter_mut() {
+            *i = *i * self.nfft_rsqrt
+        }
         result
     }
 }
@@ -45,5 +62,55 @@ impl Drop for KissFFT {
             binding::kiss_fft_free(self.cfg);
         }
         self.cfg = std::ptr::null_mut();
+    }
+}
+impl Add<Complex, Complex> for Complex {
+    fn add(self, other: Complex) -> Complex {
+        Complex { r: self.r + other.r, i: self.i + other.i }
+    }
+}
+impl Sub<Complex, Complex> for Complex {
+    fn sub(self, other: Complex) -> Complex {
+        Complex { r: self.r - other.r, i: self.i - other.i }
+    }
+}
+impl Neg<Complex> for Complex {
+    fn neg(self) -> Complex {
+        Complex { r: -self.r, i: -self.i }
+    }
+}
+impl Mul<Complex, Complex> for Complex {
+    fn mul(self, other: Complex) -> Complex {
+        Complex {
+            r: self.r*other.r - self.i*other.i,
+            i: self.r*other.i + self.i*other.r
+        }
+    }
+}
+impl Mul<Scalar, Complex> for Complex {
+    fn mul(self, other: Scalar) -> Complex {
+        Complex { r: self.r*other, i: self.i*other }
+    }
+}
+impl Div<Complex, Complex> for Complex {
+    fn div(self, other: Complex) -> Complex {
+        let denominator = other.r*other.r - other.i*other.i;
+        Complex {
+            r: (self.r*other.r + self.i*other.i)/denominator,
+            i: (self.i*other.r - self.r*other.i)/denominator
+        }
+    }
+}
+impl Div<Scalar, Complex> for Complex {
+    fn div(self, other: Scalar) -> Complex {
+        Complex { r: self.r/other, i: self.i/other }
+    }
+}
+impl Complex {
+    pub fn abs(self) -> Scalar {
+        std::num::FloatMath::hypot(self.r, self.i)
+    }
+    pub fn arg(self) -> Scalar {
+        std::num::FloatMath::atan2(self.i, self.r)
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -4,18 +4,17 @@ use std;
 use interface;
 
 #[test]
-fn test_a() {
-    let nfft_sqrt = 32u;
-    let nfft = nfft_sqrt * nfft_sqrt;
+fn test_random_fft() {
+    let nfft = 1024;
     let mut kiss_fft = interface::KissFFT::new(nfft, false);
     let mut kiss_ifft = interface::KissFFT::new(nfft, true);
-    let fin = Vec::from_fn(nfft, |_| interface::Complex {r: std::rand::random::<interface::Scalar>(), i: std::rand::random::<interface::Scalar>()} );
-    let fout_fft = kiss_fft.transform_to_vec(fin.as_slice()) .iter().map(|&x| interface::Complex {r: x.r/32., i: x.i/32.}).collect::<Vec<interface::Complex>>();
-    let fout_ifft = kiss_ifft.transform_to_vec(fout_fft.as_slice()) .iter().map(|&x| interface::Complex {r: x.r/32., i: x.i/32.}).collect::<Vec<interface::Complex>>();
+    let fin = Vec::from_fn(nfft, |_| interface::Complex {r: std::rand::random::<interface::Scalar>()*2.-1., i: std::rand::random::<interface::Scalar>()*2.-1.} );
+    let fout_fft = kiss_fft.transform_norm_to_vec(fin.as_slice());
+    let fout_ifft = kiss_ifft.transform_norm_to_vec(fout_fft.as_slice());
     println!("\nIN   = [\n{}\n]", fin.iter().map(|x: &interface::Complex| format!("    {}", x)).collect::<Vec<String>>().connect(",\n"));
     println!("\nFFT  = [\n{}\n]", fout_fft.iter().map(|x: &interface::Complex| format!("    {}", x)).collect::<Vec<String>>().connect(",\n"));
     println!("\nIFFT = [\n{}\n]", fout_ifft.iter().map(|x: &interface::Complex| format!("    {}", x)).collect::<Vec<String>>().connect(",\n"));
     for (i, j) in fin.iter().zip(fout_ifft.iter()) {
-        assert!((std::num::FloatMath::abs_sub(i.r, j.r) >= 0.01) || (std::num::FloatMath::abs_sub(i.i, j.i) >= 0.01));
+        assert!((*i - *j).abs() < 0.001);
     }
 }

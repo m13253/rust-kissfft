5579471bdcffd8e26d16f6713c99da6540293463
Allen Welkie <allen@localhost.localdomain>
2015-03-31 19:19:34 -0500

    Fixed nightly bugs, and moved to use gcc crate for building kiss_fft

--- build.rs
+++ build.rs
@@ -12,30 +12,8 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+extern crate gcc;
+
 fn main() {
-/* Note:
-   The GCC crate ( https://crates.io/crates/gcc ) is currently somewhat buggy,
-   I will switch to it only when it is stable enough. */
-    let out_dir = std::os::getenv("OUT_DIR").unwrap_or_else(|| ".".to_string());
-    let cc = std::os::getenv("CC").unwrap_or_else(|| "gcc".to_string());
-    let ar = std::os::getenv("AR").unwrap_or_else(|| "ar".to_string());
-    let compile_object = |filename: &str|
-        std::old_io::Command::new(&cc)
-            .args(&["-c", "-fPIC", "-O3", "-Wall", "-o"])
-            .args(&[format!("{}/{}.o", out_dir, filename), format!("src/{}.c", filename)])
-            .stdin(std::old_io::process::InheritFd(0)).stdout(std::old_io::process::InheritFd(1)).stderr(std::old_io::process::InheritFd(2))
-            .status();
-    let objects = ["kiss_fft", "kiss_fft_free"];
-    for object in objects.iter() {
-        assert!(compile_object(*object).unwrap().success());
-    }
-    let create_archive = |archive: &str, objects: &[&str]| {
-        std::old_io::Command::new(&ar)
-            .args(&["crs", &*format!("{}/lib{}.a", out_dir, archive)])
-            .args(&*objects.iter().map(|object: &&str| -> String format!("{}/{}.o", out_dir, *object)).collect::<Vec<String>>())
-            .stdin(std::old_io::process::InheritFd(0)).stdout(std::old_io::process::InheritFd(1)).stderr(std::old_io::process::InheritFd(2))
-            .status()
-    };
-    assert!(create_archive("kissfft", &objects).unwrap().success());
-    println!("cargo:rustc-flags=-L {} -l kissfft:static", out_dir);
+    gcc::compile_library("libkiss_fft.a", &["src/kiss_fft.c", "src/kiss_fft_free.c"]);
 }
